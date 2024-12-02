3b768d26e057bf12e57d13faece8807818db9b54
m0k0 <michael.jagroep@gmail.com>
2024-12-02 22:57:37 +0000

    day 2 part 2 solution

--- advent-2024/Solvers/Day02Solver.cs
+++ advent-2024/Solvers/Day02Solver.cs
@@ -36,9 +36,21 @@ public class Day02Solver : ISolver
                 return Result.Fail(
                     $"Empty report on line: {lineNumber}");
             }
-            if (TestReportSafety(report, useDampener))
+            if (TestReportSafety(report))
             {
                 safeReportCount++;
+            }
+            else if (useDampener)
+            {
+                var dampenedReports = ProduceDampenedReports(report);
+                foreach (var dampenedReport in dampenedReports)
+                {
+                    if (TestReportSafety(dampenedReport))
+                    {
+                        safeReportCount++;
+                        break;
+                    }
+                }
             }
         
             line = _inputReader.ReadLine();
@@ -48,13 +60,34 @@ public class Day02Solver : ISolver
         return Result.Ok(safeReportCount.ToString());
     }
 
-    
-    private bool TestReportSafety(IEnumerable<int> report, bool useDampener = false)
+    private static IEnumerable<IEnumerable<int>> ProduceDampenedReports(List<int> report)
+    {
+        var numberCount = report.Count;
+        List<List<int>> dampenedReports = [];
+
+        for (var i = 0; i < numberCount; i++)
+        {
+            List<int> dampenedReport = [];
+
+            for (var i2 = 0; i2 < numberCount; i2++)
+            {
+                if (i == i2)
+                    continue;
+                
+                dampenedReport.Add(report[i2]);
+            }
+            dampenedReports.Add(dampenedReport);
+            
+        }
+        return dampenedReports;
+    }
+
+
+    private bool TestReportSafety(IEnumerable<int> report)
     {
         bool isSafe = true;
         int? lastNumber = null;
         bool? isAscending = null;
-        bool hasDampener = useDampener;
         foreach (var number in report)
         {
             if (lastNumber is null)
@@ -71,30 +104,16 @@ public class Day02Solver : ISolver
             if (isAscending.Value && lastNumber >= number ||
                 !isAscending.Value && lastNumber <= number)
             {
-                if (hasDampener)
-                {
-                    hasDampener = false;
-                }
-                else
-                {
-                    isSafe = false;
-                    break;                    
-                }
+                isSafe = false;
+                break;
             }
             
             // rule 2: adjacent number differ by at least one, at most three
             var difference = Math.Abs(number - lastNumber.Value);
             if (difference < 1 || difference > 3)
             {
-                if (hasDampener)
-                {
-                    hasDampener = false;
-                }
-                else
-                {
-                    isSafe = false;
-                    break;                    
-                }
+                isSafe = false;
+                break;
             }
             
             lastNumber = number;
