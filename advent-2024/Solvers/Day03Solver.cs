2f86db89066586e6f3e2654dd0f3e885b416a7d8
m0k0 <michael.jagroep@gmail.com>
2024-12-03 09:32:17 +0000

    day 3 setup

---
+++ advent-2024/Solvers/Day03Solver.cs
@@ -0,0 +1,16 @@
+namespace AdventOfCode.Y2K24.Solvers;
+
+public class Day03Solver : ISolver
+{
+    private readonly TextReader _inputReader;
+    public Day03Solver(TextReader inputReader)
+    {
+        _inputReader = inputReader;
+    }
+
+
+    public Result Solve(SolutionVariant? variant)
+    {
+        return Result.Fail("No solution yet");
+    }
+}
