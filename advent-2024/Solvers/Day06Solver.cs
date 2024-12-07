323bc7a85cf5b90a7d3abbd58722fbe454da7066
m0k0 <michael.jagroep@gmail.com>
2024-12-07 18:35:30 +0000

    day 6 setup

---
+++ advent-2024/Solvers/Day06Solver.cs
@@ -0,0 +1,16 @@
+namespace AdventOfCode.Y2K24.Solvers;
+
+[SolverFor(day: 6)]
+public class Day06Solver : ISolver
+{
+    private readonly TextReader _inputReader;
+    public Day06Solver(TextReader inputReader)
+    {
+        _inputReader = inputReader;
+    }
+
+    public Result Solve(SolutionVariant? variant)
+    {
+        return Result.Fail("No solution found");
+    }
+}
