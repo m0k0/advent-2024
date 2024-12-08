26e454e95cb29cacdd975d6d15b6b9343903895e
m0k0 <michael.jagroep@gmail.com>
2024-12-08 12:59:36 +0000

    day 8 setup

---
+++ advent-2024/Solvers/Day08Solver.cs
@@ -0,0 +1,17 @@
+namespace AdventOfCode.Y2K24.Solvers;
+
+[SolverFor(day: 8)]
+public class Day08Solver: ISolver
+{
+    private readonly TextReader _inputReader;
+
+    public Day08Solver(TextReader inputReader)
+    {
+        _inputReader = inputReader;
+    }
+
+    public Result Solve(SolutionVariant? variant)
+    {
+        return Result.Fail("No solution found");
+    }
+}
