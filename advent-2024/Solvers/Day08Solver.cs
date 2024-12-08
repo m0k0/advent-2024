998636a1db5026d327b34071bb125534bb9419e9
m0k0 <michael.jagroep@gmail.com>
2024-12-08 16:18:03 +0000

    day 8 part 1 solution

--- advent-2024/Solvers/Day08Solver.cs
+++ advent-2024/Solvers/Day08Solver.cs
@@ -1,3 +1,6 @@
+using System.Drawing;
+using AdventOfCode.Y2K24.Utilities;
+
 namespace AdventOfCode.Y2K24.Solvers;
 
 [SolverFor(day: 8)]
@@ -8,10 +11,58 @@ public class Day08Solver: ISolver
     public Day08Solver(TextReader inputReader)
     {
         _inputReader = inputReader;
+        
     }
 
     public Result Solve(SolutionVariant? variant)
     {
-        return Result.Fail("No solution found");
+        var map = AdventMap2D.FromReader(_inputReader);
+        
+        if (map is null)
+            return Result.Fail("Failed to read map");
+
+        var antiNodeMap = map.Filter([]);
+        
+        var antennaGroups = map
+            .Where((MapPoint<char?> p)=>p.Value is not null)
+            .GroupBy(p => p.Value);
+
+        foreach (var group in antennaGroups)
+        {
+
+            var antennae = group.ToArray();
+            for (var i = 0; i < antennae.Length; i++)
+            {
+                var sourceAntenna = antennae[i];
+                
+                for (var i2 = 0; i2 < antennae.Length; i2++)
+                {
+                    if (i == i2)
+                        continue;
+
+                    var targetAntenna = antennae[i2];
+
+
+                    var antiNodeX =  targetAntenna.Location.X - (sourceAntenna.Location.X - targetAntenna.Location.X);
+                    var antiNodeY =  targetAntenna.Location.Y - (sourceAntenna.Location.Y - targetAntenna.Location.Y);
+
+                    if (antiNodeMap.IsInside(antiNodeX, antiNodeY))
+                        antiNodeMap[antiNodeX, antiNodeY] = '#';
+                }
+            }
+            
+        }
+        
+        foreach(var outline in AdventMap2D.ToStringLines(map))
+            Console.WriteLine(outline);
+        
+        Console.WriteLine();
+
+        foreach(var outline in AdventMap2D.ToStringLines(antiNodeMap))
+            Console.WriteLine(outline);
+
+        var antiNodeCount = antiNodeMap.Count((char? p) => p is not null);
+        
+        return Result.Ok(antiNodeCount.ToString());
     }
 }
