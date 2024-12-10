d06902ed6e034164aa70ba4975e92775d06b7b8b
m0k0 <michael.jagroep@gmail.com>
2024-12-10 00:07:38 +0000

    day 9 part 2 wip

--- advent-2024/Solvers/Day09Solver.cs
+++ advent-2024/Solvers/Day09Solver.cs
@@ -5,11 +5,21 @@ namespace AdventOfCode.Y2K24.Solvers;
 [SolverFor(day: 9)]
 public class Day09Solver :ISolver
 {
-    struct BlockSummary
+    const bool DEBUG = false;
+    class BlockSummary
     {
-        public int?[] Blocks { get; init; }
+        public int?[] Blocks { get; init; } = [];
         public int FreeBlocks { get; init; }
         public int LastFile { get; init; }
+        public DiskSegment[] FileSegments { get; init; } = [];
+        public DiskSegment[] SpaceSegments { get; init; } = [];
+
+    }
+
+    class DiskSegment(int blockIndex)
+    {
+        public int BlockIndex { get; set; } = blockIndex;
+        public int Size { get; set; }
     }
     
     private readonly TextReader _inputReader;
@@ -36,7 +46,10 @@ public class Day09Solver :ISolver
 
         var summary = GetBlockSummary(lineParts);
         
-        DefragBlocks(summary.Blocks);
+        if (variant == SolutionVariant.PartOne)
+            DefragBlocks(summary.Blocks);
+        else if (variant == SolutionVariant.PartTwo)
+            DefragBlocksBySegment(summary.Blocks, summary.FileSegments, summary.SpaceSegments);
 
         var checksum = CalculateChecksum(summary.Blocks);
         
@@ -51,12 +64,71 @@ public class Day09Solver :ISolver
         {
             var block = blocks[blockIndex];
             if (block == null)
-                break;
+                continue;
             
             checksum += block.Value * blockIndex;
         }
         
         return checksum;
+    }
+
+    private void DefragBlocksBySegment(
+        int?[] blocks, 
+        DiskSegment[] fileSegments, 
+        DiskSegment[] spaceSegments)
+    {
+        
+        DiskSegment? FindFreeSpace(int blockSize, int rangeStart, int rangeEnd)
+        {
+            for (var i = 0; i < spaceSegments.Length; i++)
+            {
+                var segment = spaceSegments[i];
+                if (segment.BlockIndex < rangeStart || segment.BlockIndex > rangeEnd)
+                    continue;
+                
+                if (segment.Size >= blockSize)
+                    return segment;
+            }
+
+            return null;
+        }
+
+        void MoveFile(DiskSegment from, DiskSegment to)
+        {
+            var sourceIndex = from.BlockIndex;
+            var targetIndex = to.BlockIndex;
+
+            for (var i = 0; i < from.Size; i++)
+            {
+                blocks[targetIndex + i] = blocks[sourceIndex + i];
+                blocks[sourceIndex + i] = null;
+            }
+            from.BlockIndex = targetIndex;
+            to.BlockIndex = sourceIndex;
+            
+        }
+
+        for (var i = fileSegments.Length -1; i >= 0; i--)
+        {
+            var fileSegment = fileSegments[i];
+
+            var freeSegment = FindFreeSpace(fileSegment.Size, 0 , fileSegment.BlockIndex);
+            if (freeSegment is null)
+                continue;
+
+            MoveFile(fileSegment, freeSegment);
+
+            PrintFileSystem(blocks);
+        }
+    }
+
+    private void PrintFileSystem(int?[] blocks)
+    {
+        if(!DEBUG)
+            return;
+        var output = string.Join("",
+            blocks.Select(b => b.HasValue ? DigitToChar(b.Value): '.'));
+        Console.WriteLine(output);
     }
 
     private void DefragBlocks(int?[] blocks)
@@ -82,47 +154,59 @@ public class Day09Solver :ISolver
                 
             blocks[leftIndex] = block;
             blocks[rightIndex] = null;
+            
+            PrintFileSystem(blocks);
         }
     }
 
     BlockSummary GetBlockSummary(IEnumerable<char> diskMap)
     {
-        var lastFileId = 0;
-        var freeSpaceCount = 0;
         var blocks = new List<int?>();
+        var files = new List<DiskSegment>();
+        var freeSpaces = new List<DiskSegment>();
             
         // get blocks
-        var diskIndex = 0;
+        var blockIndex = 0;
+        var diskIndex = -1;
         bool IsFile() => diskIndex % 2 == 0; 
         foreach (var c in diskMap)
         {
+            diskIndex++;
+            var segment = new DiskSegment(blockIndex);
             var blockSize = CharToDigit(c);
+            
             for (var i2 = 0; i2 < blockSize; i2++)
             {
                 if (IsFile())
                 {
-                    blocks.Add(lastFileId);
+                    blocks.Add(files.Count);
                 }
                 else
                 {
                     blocks.Add(null);
-                    freeSpaceCount++;
                 }
+                segment.Size++;
             }
 
             if (IsFile())
             {
-                lastFileId++;
+                files.Add(segment);
+            }
+            else
+            {
+                freeSpaces.Add(segment);
             }
 
-            diskIndex++;
+            blockIndex += blockSize;
+            
+            PrintFileSystem(blocks.ToArray());
         }
 
         return new BlockSummary()
         {
             Blocks = blocks.ToArray(),
-            FreeBlocks = freeSpaceCount,
-            LastFile = lastFileId,
+            FileSegments = files.ToArray(),
+            SpaceSegments = freeSpaces.ToArray()
         };
     }
 }
