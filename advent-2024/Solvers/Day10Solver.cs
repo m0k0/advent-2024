cf0692bef3323079c1dedfc3e0f8cf135084b603
m0k0 <michael.jagroep@gmail.com>
2024-12-10 23:44:05 +0000

    day 10 part 1 solution

--- advent-2024/Solvers/Day10Solver.cs
+++ advent-2024/Solvers/Day10Solver.cs
@@ -1,3 +1,4 @@
+using System.Drawing;
 using AdventOfCode.Y2K24.Utilities;
 
 namespace AdventOfCode.Y2K24.Solvers;
@@ -19,9 +20,74 @@ public class Day10Solver : ISolver
     {
         var map = Map2D.FromReader(
             _inputReader, (char v) => v.ToDigit());
+
         
+        if (map is null)
+            return Result.Fail("Unable to parse map");
         
+        var trailMap = map.Filter([]);
         
-        return Result.Fail("No solution found");
+        var trailHeads = map.Where(
+            (MapPoint<int?> p) => p.Value == 0);
+
+        var scoreSum = 0;
+        foreach (var trailHead in trailHeads)
+        {
+            var trail = TraverseTrailFrom(
+                map, trailHead.Location.X, trailHead.Location.Y);
+                //.ToList();
+                
+                var trailScore = trail.Where(p => p.Value == 9)
+                    .Select(p => p.Location)
+                    .Distinct()
+                    .Count();
+                
+                scoreSum += trailScore;
+                /*
+                foreach (var point in trail)
+                {
+                    trailMap[point.Location.X, point.Location.Y] = point.Value;
+                    foreach(var outline in trailMap.ToStringLines((int? value) => 
+                                value?.DigitToChar() ?? '.' ))
+                        Console.WriteLine(outline);
+                    continue;
+                }*/
+            continue;
+        }
+        
+        return Result.Ok(scoreSum.ToString());
+    }
+
+    private IEnumerable<MapPoint<int?>> TraverseTrailFrom(Map2D<int?> map, int x = 0, int y = 0)
+    {
+        Point[] movementVectors = [
+            new(0,1),
+            new(0,-1),
+            new(1,0),
+            new(-1,0)
+        ];
+
+
+        var currentHeight = map[x,y];
+        yield return new MapPoint<int?>(currentHeight, new Point(x, y));
+        
+        foreach (var movement in movementVectors)
+        {
+            var nextX = movement.X + x;
+            var nextY = movement.Y + y;
+
+            if (!map.IsInside(nextX, nextY))
+                continue;
+            
+            var nextStep = map[nextX, nextY];
+
+            if (nextStep != currentHeight + 1)
+                continue;
+            
+            foreach (var point in TraverseTrailFrom(map, nextX, nextY))
+                yield return point;
+        }
+
+        yield break;
     }
 }
