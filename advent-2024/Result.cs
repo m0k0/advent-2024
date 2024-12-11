d97e7b91cbf0a42b82bca3d9108a3eab94ce0c14
m0k0 <michael.jagroep@gmail.com>
2024-12-11 23:14:16 +0000

    refine result types

--- advent-2024/Result.cs
+++ advent-2024/Result.cs
@@ -1,10 +1,20 @@
 namespace AdventOfCode.Y2K24;
 
-
+public class SuccessResult: Result
+{
+    public SuccessResult()
+    {
+        IsSuccess = true;
+    }
+}
 public class SuccessResult<T> : Result
 {
     public T? Value { get; set; }
-    
+
+    public SuccessResult()
+    {
+        IsSuccess = true;
+    }
     public override string ToString()
     {
         return this.Value?.ToString() ?? string.Empty;
@@ -15,7 +25,11 @@ public class FailureResult : Result
 {
     public string? FailureReason { get; set; }
     public FailureResult? InnerResult { get; set; }
-    
+
+    public FailureResult()
+    {
+        IsSuccess = false;
+    }
     public override string ToString()
     {
         return this.FailureReason?.ToString() ?? string.Empty;
@@ -31,17 +45,21 @@ public class Result
     {
         return new FailureResult()
         {
-            IsSuccess = false,
             FailureReason = reason,
             InnerResult = innerResult
         };
     }
+
+    public static SuccessResult Ok()
+    {
+        return new SuccessResult();
+    }
+
     public static SuccessResult<TResult> Ok<TResult>(
         TResult value)
     {
         return new SuccessResult<TResult>()
         {
-            IsSuccess = true,
             Value = value
         };
     }
